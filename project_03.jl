322a8deb7f4aaa6e3a8f75f55ea171fadae4d1a0
Ryan C. Cooper <ryan.c.cooper@uconn.edu>
2026-04-21 15:02:01 -0400

    initial commit for project_03

---
+++ project_03.jl
@@ -0,0 +1,49 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ f17103ea-06bf-11f1-a2b0-79e68ed152eb
+md"""# Project_02 - Multibody kinematic modeling
+
+![]()
+
+In this project, a rigid bar is connected to a sliding block along a
+horizontal tracks. The sliding block is connected to a spring that stretches and compresses.  
+
+1. $x_1-y_1-$ describes block 1 position and orientation, $\theta_1$
+3. $x_2-y_2-$ describes the rigid bar position and orientation, $\theta_2$
+
+ 
+In this project, you need to 
+
+1. determine constraint equations $C(\mathbf{q},~t)$
+2. Create an augmented solution method for the dynamic motion of these two moving parts
+3. visualize the motion of the system as the two parts complete at least one oscillation
+"""
+
+# ╔═╡ 0d9be664-d7c5-4084-add2-25e5418742d6
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.4"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─f17103ea-06bf-11f1-a2b0-79e68ed152eb
+# ╠═0d9be664-d7c5-4084-add2-25e5418742d6
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
